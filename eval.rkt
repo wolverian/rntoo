c4dd7f131b8dae07c232b4ec8f996a24ff8451fc
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-12-20 17:51:53 +0200

    eval a bit

--- eval.rkt
+++ eval.rkt
@@ -3,16 +3,35 @@
 (require "parse.rkt"
          "ast.rkt")
 
-(define (rneval s)
-  (rneval* (parse-one (open-input-string s))))
+(define (todo . args)
+  (apply error args))
 
-(define (rneval* exp)
+(define (rneval s)
+  (rneval* (parse-one (open-input-string s)) initial-env))
+
+(define current-context (literal "current-context"))
+
+(define (rneval* exp env)
   (match exp
-    [(literal v) v]
-    [(call x (message "plus" (list y))) (+ (rneval* x) (rneval* y))]))
+    [(literal v)
+     v]
+    [(message msg args)
+     (rneval* (call current-context (message msg args)) env)]
+    [(call receiver (message msg args))
+     (apply (lookup msg env)
+            (rneval* receiver env)
+            (map (λ (a) (rneval* a env)) args))]))
+
+(define (lookup n env)
+  (second (assoc n env)))
+
+(define initial-env
+  (list (list "plus" +)
+        (list "assign" todo)))
 
 (module+ test
   (require rackunit)
 
   (check-equal? (rneval "42") 42)
-  (check-equal? (rneval "42 + 22") 64))
+  (check-equal? (rneval "42 + 22") 64)
+  (check-equal? (rneval "foo = 42") void))
