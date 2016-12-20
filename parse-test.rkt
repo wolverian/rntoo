e3d6d3d7b92961190430eb56758b4773a6d4c24e
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-12-20 17:03:39 +0200

    cleanup

--- parse-test.rkt
+++ parse-test.rkt
@@ -7,11 +7,14 @@
 (define (parses in expected)
   (check-equal? (parse-one (open-input-string in)) expected))
 
+(define (parses-equal a b)
+  (check-equal? (parse-one (open-input-string a))
+                (parse-one (open-input-string b))))
+
 (parses "42" (literal 42))
 (parses "\"foo\"" (literal "\"foo\""))
 (parses "foo" (message "foo" null))
 (parses "42 foo" (call (literal 42) (message "foo" null)))
 (parses "42 + 22" (call (literal 42) (message "plus" (list (literal 22)))))
 
-(check-equal? (parse-one (open-input-string "foo = method(x, x)"))
-              (parse-one (open-input-string "assign(\"foo\", method(x, x))")))
+(parses-equal "foo = method(x, x)" "assign(\"foo\", method(x, x))")
