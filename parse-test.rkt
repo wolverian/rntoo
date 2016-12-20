172c0680e0b0ffab319832993d97e5b9fc74670f
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-12-20 17:04:36 +0200

    cleanup vol 2

--- parse-test.rkt
+++ parse-test.rkt
@@ -4,18 +4,18 @@
          "parse.rkt"
          "ast.rkt")
 
-(define (parses in expected)
+(define (parses-as? in expected)
   (check-equal? (parse-one (open-input-string in)) expected))
 
-(define (parses-equal a b)
+(define (parses-equal? a b)
   (check-equal? (parse-one (open-input-string a))
                 (parse-one (open-input-string b))))
 
-(parses "42" (literal 42))
-(parses "\"foo\"" (literal "\"foo\""))
-(parses "foo" (message "foo" null))
-(parses "42 foo" (call (literal 42) (message "foo" null)))
-(parses "42 + 22" (call (literal 42) (message "plus" (list (literal 22)))))
+(parses-as? "42" (literal 42))
+(parses-as? "\"foo\"" (literal "\"foo\""))
+(parses-as? "foo" (message "foo" null))
+(parses-as? "42 foo" (call (literal 42) (message "foo" null)))
+(parses-as? "42 + 22" (call (literal 42) (message "plus" (list (literal 22)))))
 
-(parses-equal "foo = method(x, x)" "assign(\"foo\", method(x, x))")
-(parses-equal "42 + foo" "42 plus(foo)")
+(parses-equal? "foo = method(x, x)" "assign(\"foo\", method(x, x))")
+(parses-equal? "42 + foo" "42 plus(foo)")
