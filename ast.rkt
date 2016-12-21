cd011ea835d700ab47bb8e7c16e2be8d559ed8fe
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2016-12-21 21:32:50 +0200

    cleanup again too

--- ast.rkt
+++ ast.rkt
@@ -4,9 +4,9 @@
 
 (provide (all-defined-out))
 
-(struct message ((name : String) (args : (Listof Expr))) #:transparent)
-(struct send ((receiver : Expr) (msg : message)) #:transparent)
-(struct literal ((value : (U Number String))) #:transparent)
+(struct message ([name : String] [args : (Listof Expr)]) #:transparent)
+(struct send ([receiver : Expr] [msg : message]) #:transparent)
+(struct literal ([value : (U Number String)]) #:transparent)
 
 (define-type Expr (U message send literal))
 (define-predicate Expr? Expr)
