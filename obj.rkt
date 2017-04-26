47eb2df9093cc3c8bfa1cefc71a8a1ee4349e865
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2017-04-26 19:24:29 +0300

    the great obj

---
+++ obj.rkt
@@ -0,0 +1,67 @@
+#lang racket
+
+(require threading)
+
+; primitive types
+
+(struct obj (slots proto) #:transparent)
+(struct native-fun (fun) #:transparent)
+
+(define (slot name value)
+  (list name value))
+
+(define slot-name first)
+(define slot-value second)
+
+(define (slot-lookup target name)
+  (~>> target
+       obj-slots
+       (assoc name)
+       second))
+
+; object types
+
+(define (message name args)
+  (obj (list (slot "name" name)
+             (slot "args" args))
+       initial))
+
+(define (call target message)
+  (obj (list (slot "target" target)
+             (slot "message" message))
+       initial))
+
+(define (native-list values)
+  (obj (list (slot "values" values)) initial))
+
+(define (object)
+  (obj (list (slot "slotNames"
+                   (native-fun
+                    (λ (self) (~>> self
+                                   obj-slots
+                                   (map slot-name)
+                                   native-list)))))
+       null))
+
+(define initial (object))
+
+(define (block scope code)
+  (obj (list (slot "scope" scope)
+             (slot "code" code))
+       initial))
+
+; do a call!
+
+(define (do-call call)
+  (let* ([target (slot-lookup call "target")]
+         [message (slot-lookup call "message")]
+         [name (slot-lookup message "name")]
+         [code (slot-lookup target name)])
+    (if (native-fun? code)
+        ((native-fun-fun code) target)
+        (error "unimplemented"))))
+
+(module+ test
+  (require typed/rackunit)
+  (check-equal? (do-call (call initial (message "slotNames" null)))
+                (native-list (list "slotNames"))))
